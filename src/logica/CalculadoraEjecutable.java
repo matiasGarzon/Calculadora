8ddb8592754e45ebaa7027f1f7be82a468428cca
berna <berna@Bernardo>
2025-08-22 18:24:53 -0300

    clase 22/08/25

---
+++ src/logica/CalculadoraEjecutable.java
@@ -0,0 +1,12 @@
+
+package logica;
+
+import calculadoraGui.CalculadoraGui;
+
+public class CalculadoraEjecutable {
+    public static void main(String[] args) {
+        CalculadoraGui calcu = new CalculadoraGui();
+        calcu.setVisible(true);
+        calcu.setLocationRelativeTo(null);
+    }
+}
